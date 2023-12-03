fe02c062e2056f78aad22196388041203f2dce8d
ChazWinterYT <chaz@hmrmusic.com>
2023-12-03 11:04:19 -0500

    Solution for Day 3 completed, with documentation

---
+++ src/main/java/com/chazwinter/Day03.java
@@ -0,0 +1,252 @@
+package com.chazwinter;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day03 {
+    public static final int PUZZLE_SIZE = 140;   // 10 for test input, 140 for full input
+    char[][] engineGrid = new char[PUZZLE_SIZE][PUZZLE_SIZE];
+    /* Two arrays to represent where a neighbor cell is relative to the current one.
+        For example, the top-left cell is row-1 and col-1. */
+    int[] dRow = new int[] {-1, -1, -1,
+            0,      0,
+            1,  1,  1};
+    int[] dCol = new int[] {-1,  0,  1,
+            -1,      1,
+            -1,  0,  1};
+
+    public int machineParts(String filePath, int part) throws IOException {
+        // Store puzzle input as a 2D char array (DID YOU SET THE PUZZLE SIZE?).
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line;
+        int lineNumber = 0;
+        while ((line = reader.readLine()) != null) {
+            for (int i = 0; i < line.length(); i++) {
+                engineGrid[lineNumber][i] = line.charAt(i);
+            }
+            lineNumber++;
+        }
+
+        if (part == 1) {
+            // If we see a digit, check if it has symbols around it.
+            int sumOfEngineParts = 0;
+            for (int i = 0; i < engineGrid.length; i++) {
+                for (int j = 0; j < engineGrid[0].length; j++) {
+                    if (!Character.isDigit(engineGrid[i][j])) {
+                        continue;
+                    }
+                    sumOfEngineParts += searchForSymbols(i, j);
+                }
+            }
+            return sumOfEngineParts;
+        }
+        if (part == 2) {
+            int sumOfGearProducts = 0;
+            /* If we see a gear, check if it has two numbers around it.
+            * We can't destroy the numbers this time, so copy the surrounding rows
+            * into new Lists, so we can destroy THOSE instead. */
+            for (int i = 0; i < engineGrid.length; i++) {
+                for (int j = 0; j < engineGrid[0].length; j++) {
+                    if (engineGrid[i][j] == '*') {
+                        char[][] rowsAroundGear = getRowsAroundGear(i, j);
+                        List<Integer> numsAroundGear = searchForNumbers(rowsAroundGear, 1, j);
+                        // System.out.println(numsAroundGear);
+                        if (numsAroundGear.size() == 2) {
+                            int product = numsAroundGear.get(0) * numsAroundGear.get(1);
+                            sumOfGearProducts += product;
+                        }
+                    }
+                }
+            }
+            return sumOfGearProducts;
+        }
+        return -1;
+    }
+
+    /**
+     * Search the grid for symbols (not a digit and not a period).
+     * Only works for part 1. searchForDigits() is the part 2 equivalent.
+     * @return The number near that symbol, if there is one.
+     */
+    private int searchForSymbols(int row, int col) {
+        int valueToAdd = 0;
+        for (int i = 0; i < dRow.length; i++) {
+            int newRow = row + dRow[i];
+            int newCol = col + dCol[i];
+            if (isInBounds(newRow, newCol) && isASymbol(newRow, newCol)) {
+                // System.out.printf("Symbol %c found near value %c (row %d, col %d)\n",
+                //        engineGrid[newRow][newCol], engineGrid[row][col], row, col);
+                valueToAdd = getValueFromGrid(engineGrid[row], row, col);
+            }
+        }
+        return valueToAdd;
+    }
+
+    /**
+     * Helper method to determine if a cell is in bounds before taking action on it.
+     * @param row The row of the cell to check.
+     * @param col the column of the cell the check.
+     * @return true if the cell is in bounds, false if it is not.
+     */
+    private boolean isInBounds(int row, int col) {
+        // DID YOU SET THE PUZZLE SIZE?
+        return row >= 0 && row < PUZZLE_SIZE
+                && col >= 0 && col < PUZZLE_SIZE;
+    }
+
+    /**
+     * Helper method to determine if a character in a cell is a symbol (not a digit or a period).
+     */
+    private boolean isASymbol(int row, int col) {
+        char symbol = engineGrid[row][col];
+        return !Character.isDigit(symbol) && symbol != '.';
+    }
+
+    /**
+     * When a digit is found in the grid, get the full number associated
+     * with that digit. This only works for Part 1; This method is overloaded
+     * later on for Part 2.
+     * @param rowChars The array where the digit was found.
+     * @param row The row of that array in the overall grid.
+     * @param col The column where the digit was found.
+     * @return The number associated with the found digit.
+     */
+    private int getValueFromGrid(char[] rowChars, int row, int col) {
+        int result = 0;
+        StringBuilder sb = new StringBuilder();
+        // Add the current digit (also destroy it, so we don't count it later on)
+        sb.append(rowChars[col]);
+        engineGrid[row][col] = '.';
+        // Get digits to the left (and destroy them, so they don't trigger this same method later)
+        int left = col - 1;
+        while (isInBounds(row, left) && isADigit(row, left)) {
+            sb.insert(0, rowChars[left]);
+            engineGrid[row][left] = '.';
+            left--;
+        }
+        // Get digits to the right (and destroy them, so they don't trigger this same method later)
+        int right = col + 1;
+        while (isInBounds(row, right) && isADigit(row, right)) {
+            sb.append(rowChars[right]);
+            engineGrid[row][right] = '.';
+            right++;
+        }
+        return Integer.parseInt(sb.toString());
+    }
+
+    /**
+     * Helper method to determine if a character in a cell is a digit.
+     */
+    private boolean isADigit(int row, int col) {
+        char symbol = engineGrid[row][col];
+        return Character.isDigit(symbol);
+    }
+
+    /**
+     * For Part 2. When a gear is found, we need a COPY of the three grid rows
+     * surrounding that gear.
+     * @param i The row where the gear was found in the overall grid.
+     * @param j The column where the gear was found in that row.
+     * @return The 3 x [PUZZLE_SIZE] array surrounding the found gear.
+     */
+    private char[][] getRowsAroundGear(int i, int j) {
+        char[][] rowsAroundGear = new char[3][PUZZLE_SIZE];
+        // Row above
+        if (isInBounds(i - 1, 0)) {
+            rowsAroundGear[0] = copyArray(engineGrid[i - 1]);
+        } else {
+            Arrays.fill(rowsAroundGear[0], '.');
+        }
+        // Row that the gear is in
+        rowsAroundGear[1] = copyArray(engineGrid[i]);
+        // Row below
+        if (isInBounds(i + 1, 0)) {
+            rowsAroundGear[2] = copyArray(engineGrid[i + 1]);
+        } else {
+            Arrays.fill(rowsAroundGear[2], '.');
+        }
+        removeAllOtherGears(rowsAroundGear, j);
+        return rowsAroundGear;
+    }
+
+    /**
+     * Helper method to produce a copy of an array.
+     */
+    private char[] copyArray(char[] rowToCopy) {
+        return Arrays.copyOf(rowToCopy, PUZZLE_SIZE);
+    }
+
+    /**
+     * When producing a copy of the array, we only want it to include
+     * the CURRENT gear, not any other gears that might coincidentally
+     * exist within these three rows.
+     * @param rowsAroundGear The copied 2D array containing our gear.
+     * @param ourGear The gear we want to keep.
+     */
+    private void removeAllOtherGears(char[][] rowsAroundGear, int ourGear) {
+        for (int i = 0; i < rowsAroundGear.length; i++) {
+            for (int j = 0; j < PUZZLE_SIZE; j++) {
+                if (i == 1 && j == ourGear) continue;    // Don't remove our gear!
+                if (rowsAroundGear[i][j] == '*') {
+                    rowsAroundGear[i][j] = '.';
+                }
+            }
+        }
+    }
+
+    /**
+     * When a gear is found, we need to find the numbers surrounding it, and
+     * add them to a List to evaluate later.
+     * @param rowsAroundGear The copy of the grid rows surrounding the gear.
+     * @param row The row where the gear is located.
+     *            Due to the copied grid's design, the gear is always in row 1.
+     * @param col The column where the gear is located.
+     * @return A List of all numbers that surround the gear.
+     */
+    private List<Integer> searchForNumbers(char[][] rowsAroundGear, int row, int col) {
+        // System.out.println(Arrays.deepToString(rowsAroundGear));
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < dRow.length; i++) {
+            int newRow = row + dRow[i];
+            int newCol = col + dCol[i];
+            if (isInBounds(newRow, newCol) && Character.isDigit(rowsAroundGear[newRow][newCol])) {
+                // System.out.printf("Digit %c (row %d, col %d) found near symbol %c\n",
+                //        rowsAroundGear[newRow][newCol], newRow, newCol, rowsAroundGear[row][col]);
+                numbers.add(getValueFromGrid(rowsAroundGear, rowsAroundGear[newRow], newRow, newCol));
+            }
+        }
+        return numbers;
+    }
+
+    /**
+     * The other getValueFromGrid() method only specifically works for part 1,
+     * and I don't want to refactor it, so this is an overloaded version
+     * that specifically works for part 2.
+     */
+    private int getValueFromGrid(char[][] rowsAroundGear, char[] rowChars, int row, int col) {
+        int result = 0;
+        StringBuilder sb = new StringBuilder();
+        // Add the current digit (also destroy it, so we don't count it later on)
+        sb.append(rowChars[col]);
+        rowsAroundGear[row][col] = '.';
+        // Get digits to the left (and destroy them, so they don't trigger this same method later)
+        int left = col - 1;
+        while (isInBounds(row, left) && Character.isDigit(rowChars[left])) {
+            sb.insert(0, rowChars[left]);
+            rowsAroundGear[row][left] = '.';
+            left--;
+        }
+        // Get digits to the right (and destroy them, so they don't trigger this same method later)
+        int right = col + 1;
+        while (isInBounds(row, right) && Character.isDigit(rowChars[right])) {
+            sb.append(rowChars[right]);
+            rowsAroundGear[row][right] = '.';
+            right++;
+        }
+        return Integer.parseInt(sb.toString());
+    }
+}
--- src/main/java/com/chazwinter/Main.java
+++ src/main/java/com/chazwinter/Main.java
@@ -6,11 +6,19 @@ import java.io.IOException;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException {
-        int day = 2;                // Which day do you want to run?
+        int day = 3;                // Which day do you want to run?
         boolean fullInput = true;  // true = full puzzle input; false = test input only
         String filePath = buildFilePathForToday(day, fullInput);
 
         /* Code for running each day. Specify day and input type above. */
+        if (day == 3) {
+            Day03 day03 = new Day03();
+            int part1Sum = day03.machineParts(filePath, 1);
+            System.out.println(part1Sum);
+            int part2Sum = day03.machineParts(filePath, 2);
+            System.out.println(part2Sum);
+        }
+
         if (day == 2) {
             Day02 day02 = new Day02();
             int part1Sum = day02.validGameTotal(filePath, 1);
@@ -28,10 +36,12 @@ public class Main {
     }
 
     private static String buildFilePathForToday(int day, boolean fullInput) {
-        String result = "src/main/resources/";
-        result += fullInput ? "input" : "test";
-        result += "_day" + String.format("%02d", day);
-        System.out.println(result);
-        return result;
+        StringBuilder sb = new StringBuilder();
+        sb.append("src/main/resources/");
+        sb.append(fullInput ? "input" : "test");
+        sb.append("_day");
+        sb.append(String.format("%02d", day));
+        // System.out.println(sb);
+        return sb.toString();
     }
 }
